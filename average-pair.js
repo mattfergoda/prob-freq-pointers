d60fdd91dc4de4d62ff7d30981b1c83c28aeecc8
Matt Fergoda <mattfergoda@users.noreply.github.com>
2023-09-15 15:27:42 -0700

    Tests passing.

--- average-pair.js
+++ average-pair.js
@@ -1,4 +1,30 @@
-// add whatever parameters you deem necessary & write docstring
-function averagePair() {
+
+/**
+ * Takes in sorted array of numbers, nums, and a numeric targetAvg.
+ * If there is a pair of numbers in nums where the average of the pair equals
+ * targetAvg, return true. Otherwise, return false.
+ */
+function averagePair(nums, targetAvg) {
+  // x declare a left pointer starting on the left side of nums.
+  let left = 0;
+  // x declare a right pointer starting on the right side of nums.
+  let right = nums.length - 1;
+  // while left < right
+  while (left < right) {
+    const avg = (nums[left] + nums[right]) / 2;
+    // x if the average of left and right equals targetAvg, return true.
+    if (avg === targetAvg) return true;
+    // x if the average of left and right > targetAvg, move right one space to the left.
+    else if (avg > targetAvg) {
+      right -= 1;
+    }
+    // x otherwise, move left one space to the right.
+    else {
+      left += 1;
+    }
+  }
+
+  // x if we make it out of the while loop, return false.
+  return false;
 }
 
--- can-construct-word.js
+++ can-construct-word.js
@@ -1,3 +1,40 @@
-// add whatever parameters you deem necessary & write doc comment
-function canConstructWord() {
+/**
+ * Accepts two strings, word and letters. If word can be made from letters,
+ * return true. Otherwise, return false. Assumes only lowercase for words
+ * and letters.
+ */
+function canConstructWord(word, letters) {
+
+  // x Build a frequency counter.
+
+  // x Get the frequencies of each letter in word.
+  const wordFreqs = getFrequencyCounts(word);
+
+  // x Get the frequencies of each letter in letter.
+  const letterFreqs = getFrequencyCounts(letters);
+
+  // x if any letter in word is not in letter freqs, or the value of that key
+  // in letter freqs is < that letter's key in word freqs, return false.
+  for (const char of word) {
+    if (!(char in letterFreqs)) return false; // Char isn't even in letters
+    if (letterFreqs[char] < wordFreqs[char]) return false;
+  }
+  // x if we get to the end of the loop, return true.
+  return true;
+}
+
+/**
+ * Takes in a string and counts the frequency of each char in the string.
+ * Returns an object where each key is a unique item and its value is the
+ * frequency.
+ */
+function getFrequencyCounts(str) {
+  let freqs = {};
+
+  for (const char of str) {
+    const count = (freqs[char] || 0) + 1;
+    freqs[char] = count;
+  }
+
+  return freqs;
 }
